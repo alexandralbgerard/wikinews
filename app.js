a649ca1193dfc63bd70c6b6d13fc8ec0a9106953
Alexandra Gerard <alexandralbgerard@gmail.com>
2019-01-18 11:06:52 -0600

    updates for kris to fork

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const volleyball = require('volleyball');
 const express = require('express');
 const app = express();
+const { db } = require('./models');
 
 app.use(volleyball);
 
@@ -10,11 +11,16 @@ app.use(staticMiddlewear);
 const bodyParser = require('body-parser');
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res, next) => {
-  console.log('hello world!');
+db.authenticate().then(() => {
+  console.log('connected to the database');
 });
 
-const PORT = 6666;
+app.get('/', (req, res, next) => {
+  console.log('hello world!');
+  res.send('');
+});
+
+const PORT = 1337;
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
